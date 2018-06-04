98cf5224642411f0663ebf476f97fece8eddd52b
Michael van Meerwijk <michaelvanm@gmail.com>
2018-06-04 22:08:32 +0200

    default user settings file

---
+++ src/store/defaultUserSettings.js
@@ -0,0 +1,19 @@
+export default {
+	name: "",
+	// [nl, en]
+	language: "nl",
+	// range: 10-20, or 100%
+	fontSize: '100%',
+	customLocation: null,
+	activeWidgets: [
+		'greeting',
+		'news',
+		'quote',
+		'weather',
+		'time'
+	],
+	// any valid unsplash collection id. maybe provide a list as well?
+	wallpaperCollection: 220388,
+	// [motivinspirational, movies, famous]
+	quoteCategory: "motivinspirational"
+}

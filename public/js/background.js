c5ec0548e4b1c6c31e4e82b818c9185630163fc0
Michael van Meerwijk <michaelvanm@gmail.com>
2018-06-17 21:59:53 +0200

    BG script better instead of BG page

--- public/js/background.js
+++ public/js/background.js
@@ -1,5 +1,13 @@
-chrome.runtime.onInstalled.addListener(details => {
-	
+chrome.runtime.onInstalled.addListener((details, prevVersion) => {
+	const curVersion = chrome.runtime.getManifest().version;
+	if (details.reason === "install") {
+		console.log("Installed!");
+		console.log(`Version ${curVersion} has been installed of Origin New Tab.`);
+	} else if (details.reason === "update") {
+		console.log("Update!");
+		console.log(`Origin New Tab has been updated from ${prevVersion} to ${curVersion}.`);
+	}
+	window.localStorage.setItem('sp_app_version', curVersion);
 });
 
 chrome.browserAction.onClicked.addListener(() => {

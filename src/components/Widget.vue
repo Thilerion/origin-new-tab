cca1fc58053ab2c1592cedb0cadac059fb681831
Michael van Meerwijk <michaelvanm@gmail.com>
2018-06-20 14:27:23 +0200

    widget settings component created

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -8,46 +8,14 @@
 			:style="{'font-size': widgetFontSize}"
 		/>
 
-		<div class="widget-settings" v-if="dndEnabled">
-			<div class="widget-setting-group widget-settings-font" v-if="canChangeFontSize">
-				<button 
-					class="widget-setting-btn decrease icon-btn"
-					:disabled="minFontSizeReached"
-					@click="decreaseFontSize"
-				>-</button>
-				<span class="setting-name">Text</span>
-				<button
-					class="widget-setting-btn increase icon-btn"
-					:disabled="maxFontSizeReached"
-					@click="increaseFontSize"
-				>+</button>
-			</div>
-
-			<div class="widget-setting-group widget-settings-width" v-if="canChangeWidth">
-				<button
-					class="widget-setting-btn decrease icon-btn"
-					@click="decreaseWidth"
-				>-</button>
-				<span class="setting-name">Width</span>
-				<button
-					class="widget-setting-btn increase icon-btn"
-					@click="increaseWidth"
-				>+</button>
-			</div>
-
-			<div class="widget-setting-group widget-settings-height" v-if="canChangeHeight">
-				<button
-					class="widget-setting-btn decrease icon-btn"
-					@click="decreaseHeight"
-				>-</button>
-				<span class="setting-name">Height</span>
-				<button
-					class="widget-setting-btn increase icon-btn"
-					@click="increaseHeight"
-				>+</button>
-			</div>
-
-		</div>
+		<StartWidgetSettings
+			class="widget-settings"
+			v-if="dndEnabled"
+			@changeFontSize="changeFontSize"
+			@changeWidth="changeWidth"
+			@changeHeight="changeHeight"
+			:widget="widget"
+		/>
 	</div>
 </template>
 
@@ -60,6 +28,7 @@ import StartNews from './widgets/News.vue';
 import StartSettingsButton from './SettingsButton.vue';
 import StartTopPages from './widgets/TopPages.vue';
 import StartCalendar from './widgets/Calendar.vue';
+import StartWidgetSettings from './WidgetSettings.vue';
 
 import {settingsOptions} from '@/store/defaultUserSettings';
 
@@ -72,7 +41,8 @@ export default {
 		StartNews,
 		StartSettingsButton,
 		StartTopPages,
-		StartCalendar
+		StartCalendar,
+		StartWidgetSettings
 	},
 
 	props: {
@@ -99,8 +69,7 @@ export default {
 
 	data() {
 		return {
-			baseFontSize: '1rem',
-			widgetOptions: settingsOptions.user.widgetOptions[this.widget.name]
+			baseFontSize: '1rem'
 		}
 	},
 
@@ -130,159 +99,51 @@ export default {
 		widgetFontSize() {
 			let fontSizeMod = this.widget.fontSize || 0;
 			return `calc(${fontSizeMod}px + ${this.baseFontSize})`;
-		},
-
-		canChangeFontSize() {
-			return !!this.widgetOptions.fontSize;
-		},
-
-		maxFontSizeReached() {
-			return this.widget.fontSize >= settingsOptions.user.widgetFontSize.max;
-		},
-
-		minFontSizeReached() {
-			return this.widget.fontSize <= settingsOptions.user.widgetFontSize.min;
-		},
-
-		canChangeHeight() {
-			return !!this.widgetOptions.height;
-		},
-
-		canChangeWidth() {
-			return !!this.widgetOptions.width;
 		}
 
 	},
 
 	methods: {
-
-		increaseFontSize() {
-			this.$store.commit('increaseFontSize', this.widget.name);
+		changeFontSize(amount) {
+			if (amount < 0) {
+				this.$store.commit('decreaseFontSize', this.widget.name);
+			} else if (amount > 0) {
+				this.$store.commit('increaseFontSize', this.widget.name);
+			}				
 		},
-
-		decreaseFontSize() {
-			this.$store.commit('decreaseFontSize', this.widget.name);
-		},
-
-		//TODO: mutation needs "gridCols" variable
-		increaseHeight() {
+		changeHeight(amount) {
 			const data = {
 				name: this.widget.name,
 				gridRows: this.gridRows,
 				widgetRows: this.rows,
-				change: 1
+				change: amount
 			};
 			this.$store.commit('changeWidgetHeight', data);
 		},
-
-		decreaseHeight() {
-			const data = {
-				name: this.widget.name,
-				gridRows: this.gridRows,
-				widgetRows: this.rows,
-				change: -1
-			};
-			this.$store.commit('changeWidgetHeight', data);
-		},
-
-		increaseWidth() {
+		changeWidth(amount) {
 			const data = {
 				name: this.widget.name,
 				gridCols: this.gridCols,
 				widgetCols: this.columns,
-				change: 1
-			};
-			this.$store.commit('changeWidgetWidth', data);
-		},
-
-		decreaseWidth() {
-			const data = {
-				name: this.widget.name,
-				gridCols: this.gridCols,
-				widgetCols: this.columns,
-				change: -1
+				change: amount
 			};
 			this.$store.commit('changeWidgetWidth', data);
 		}
-
 	}
 }
 </script>
 
 <style>
-.dnd .draggable .widget-inner {
-	/* cursor: move; */
-}
-
 .dnd .widget.draggable .widget-inner * {
 	cursor: move;
 }
 </style>
 
 <style scoped>
-.widget-settings {
-	/* background: white; */
-	color: black;
-	font-size: 12px;
-	display: flex;
+.widget-settings {	
 	position: absolute;
 	top: 0;
-	right: 0;
-	opacity: 0.2;
-	flex-direction: column;
-	align-items: stretch;
-	justify-content:stretch;
+	right: 0;	
 	max-height: 100%;
-	flex-wrap: wrap-reverse;
-}
-
-.widget:hover .widget-settings {
-	opacity: 1;
-}
-
-.widget-setting-group {
-	flex: 1 1 auto;
-	white-space: nowrap;
-	padding: 0.2em 0.3em;
-	display: flex;
-	align-items: center;
-	background: white;
-}
-
-.widget-setting-group:last-of-type {
-	border-bottom-left-radius: 4px;
-	padding-bottom: 0.3em;
-}
-
-.setting-name {
-	flex: 1 1 auto;
-	text-align: center;
-}
-
-.widget-setting-btn {
-	border: 2px solid transparent;
-	background-color: black;
-	color: white;
-	border-radius: 4px;
-	width: 1em;
-	height: 1em;
-	font-size: 14px;
-	line-height: 10px;
-	cursor: pointer;
-	transition: all .1s ease;
-}
-
-.widget-setting-btn:hover:not(:active) {
-	background: transparent;
-	color: black;
-	border: 2px solid black;
-}
-
-.widget-setting-btn.decrease {
-	margin-right: 0.2em;
-}
-
-.widget-setting-btn.increase {
-	margin-left: 0.2em;
 }
 </style>
---
+++ src/components/WidgetSettings.vue
@@ -0,0 +1,140 @@
+<template>
+	<div class="widget-settings">
+		<div class="widget-setting-group widget-settings-font" v-if="canChangeFontSize">
+			<button 
+				class="widget-setting-btn decrease icon-btn"
+				:disabled="minFontSizeReached"
+				@click="$emit('changeFontSize', -1)"
+			>-</button>
+			<span class="setting-name">Text</span>
+			<button
+				class="widget-setting-btn increase icon-btn"
+				:disabled="maxFontSizeReached"
+				@click="$emit('changeFontSize', 1)"
+			>+</button>
+		</div>
+
+		<div class="widget-setting-group widget-settings-width" v-if="canChangeWidth">
+			<button
+				class="widget-setting-btn decrease icon-btn"
+				@click="$emit('changeWidth', -1)"
+			>-</button>
+			<span class="setting-name">Width</span>
+			<button
+				class="widget-setting-btn increase icon-btn"
+				@click="$emit('changeWidth', 1)"
+			>+</button>
+		</div>
+
+		<div class="widget-setting-group widget-settings-height" v-if="canChangeHeight">
+			<button
+				class="widget-setting-btn decrease icon-btn"
+				@click="$emit('changeHeight', -1)"
+			>-</button>
+			<span class="setting-name">Height</span>
+			<button
+				class="widget-setting-btn increase icon-btn"
+				@click="$emit('changeHeight', 1)"
+			>+</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import {settingsOptions} from '@/store/defaultUserSettings';
+
+export default {
+	props: {
+		widget: {
+			type: Object,
+			required: true
+		}
+	},
+	data() {
+		return {
+			widgetOptions: settingsOptions.user.widgetOptions[this.widget.name]
+		}
+	},
+	computed: {
+		canChangeFontSize() {
+			return !!this.widgetOptions.fontSize;
+		},
+		canChangeWidth() {
+			return !!this.widgetOptions.width;
+		},
+		canChangeHeight() {
+			return !!this.widgetOptions.height;
+		},
+		minFontSizeReached() {
+			return this.widget.fontSize <= settingsOptions.user.widgetFontSize.min;
+		},
+		maxFontSizeReached() {
+			return this.widget.fontSize >= settingsOptions.user.widgetFontSize.max;
+		}
+	}
+}
+</script>
+
+<style scoped>
+.widget-settings {
+	color: black;
+	font-size: 12px;
+	display: flex;
+	opacity: 0.2;
+	flex-direction: column;
+	align-items: stretch;
+	justify-content:stretch;
+	flex-wrap: wrap-reverse;
+	transition: opacity .2s ease;
+}
+
+.widget:hover .widget-settings {
+	opacity: 1;
+}
+
+.widget-setting-group {
+	flex: 1 1 auto;
+	white-space: nowrap;
+	padding: 0.2em 0.3em;
+	display: flex;
+	align-items: center;
+	background: white;
+}
+
+.widget-setting-group:last-of-type {
+	border-bottom-left-radius: 4px;
+	padding-bottom: 0.3em;
+}
+
+.setting-name {
+	flex: 1 1 auto;
+	text-align: center;
+}
+
+.widget-setting-btn {
+	border: 2px solid transparent;
+	background-color: black;
+	color: white;
+	border-radius: 4px;
+	width: 1em;
+	height: 1em;
+	font-size: 14px;
+	line-height: 10px;
+	cursor: pointer;
+	transition: all .1s ease;
+}
+
+.widget-setting-btn:hover:not(:active) {
+	background: transparent;
+	color: black;
+	border: 2px solid black;
+}
+
+.widget-setting-btn.decrease {
+	margin-right: 0.2em;
+}
+
+.widget-setting-btn.increase {
+	margin-left: 0.2em;
+}
+</style>

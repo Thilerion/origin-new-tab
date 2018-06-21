53af621d6c78835720defa5c418d0de0ec40fd5f
Michael van Meerwijk <michaelvanm@gmail.com>
2018-06-21 17:45:21 +0200

    Styling of new settings page

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -13,25 +13,57 @@
 
 						<div class="setting-item">
 							<h3 class="setting-item-name">Naam</h3>				
-							<input type="text" v-model="currentSettings.general.username">
+							<input
+								type="text"
+								class="input-text"
+								v-model="currentSettings.general.username">
 						</div>
 
 						<div class="setting-item">
 							<h3 class="setting-item-name">Taal</h3>
-							<label v-for="lang in settingsOptions.general.language" :key="lang.id"><input type="radio" :value="lang.id" v-model="currentSettings.general.language">{{lang.name}}</label>
+							<label
+								v-for="lang in settingsOptions.general.language"
+								:key="lang.id"
+							><input
+								class="input-radio"
+								type="radio"
+								:value="lang.id"
+								v-model="currentSettings.general.language"
+							>{{lang.name}}</label>
 						</div>
 
 						<div class="setting-item">
 							<h3 class="setting-item-name">Textgrootte</h3>
 							<div class="checkbox-group">
-								<input type="radio" v-model="currentSettings.general.fontSize"><label>Standaard</label>
-							</div>				
-							<input type="range" class="slider is-circle" :class="{'range-disabled': disableFontSizeSlider}" :min="settingsOptions.general.fontSize.min" :max="settingsOptions.general.fontSize.max" v-model="currentSettings.general.fontSize"><label :class="{'hide-label': !!disableFontSizeSlider}">{{currentSettings.general.fontSize}}px</label>
+								<input
+									type="radio"
+									v-model="currentSettings.general.fontSize"
+									class="input-radio"
+								><label>Standaard</label>
+							</div>
+							<div
+								class="input-range-wrap"
+							>
+								<input
+									type="range"
+									class="slider is-circle input-range"
+									:class="{'range-disabled': disableFontSizeSlider}"
+									:min="settingsOptions.general.fontSize.min"
+									:max="settingsOptions.general.fontSize.max"
+									v-model="currentSettings.general.fontSize"
+								><label
+									:class="{'hide-label': !!disableFontSizeSlider}"
+								>{{currentSettings.general.fontSize}}px</label>
+							</div>			
 						</div>
 
 						<div class="setting-item">
 							<h3 class="setting-item-name">Laat welkomsttekst zien</h3>
-							<input type="checkbox" v-model="currentSettings.general.showTextGreeting">
+							<input
+								type="checkbox"
+								v-model="currentSettings.general.showTextGreeting"
+								class="input-checkbox"
+							>
 						</div>
 
 					</div>
@@ -43,7 +75,7 @@
 						<div class="setting-item">
 							<h3 class="setting-item-name">Actieve widgets</h3>
 							<div class="checkbox-group" v-for="widget in widgetsCanBeDisabled" :key="widget.name">
-								<input type="checkbox" v-model="widget.active"><label class="text-capitalize">{{widget.name}}</label>
+								<input type="checkbox" class="input-checkbox" v-model="widget.active"><label class="text-capitalize">{{widget.name}}</label>
 							</div>
 						</div>
 					</div>
@@ -54,10 +86,12 @@
 					<div class="setting-group-content">
 						<div class="setting-item">
 							<h3 class="setting-item-name">Gebruik aangepaste locatie</h3>
-							<div class="checkbox-group">
-								<input type="checkbox" v-model="currentSettings.weather.useCustomLocation">
-								<label v-if="currentSettings.weather.useCustomLocation">Ja: <input
+							<div class="checkbox-group checkbox-group-optional-text input-text-height">
+								<input type="checkbox" class="input-checkbox" v-model="currentSettings.weather.useCustomLocation">
+								<label v-if="currentSettings.weather.useCustomLocation"
+								>Ja: <input
 									type="text"
+									class="input-text"
 									v-model="currentSettings.weather.customLocationToUse"
 								></label>
 								<label v-else>Nee, gebruik browser locatie.</label>
@@ -71,7 +105,7 @@
 					<div class="setting-group-content">
 						<div class="setting-item">
 							<h3 class="setting-item-name">Achtergrond collectie</h3>
-							<div class="select">
+							<div class="input-select">
 								<select
 									v-model="currentSettings.wallpaper.wallpaperCollection"
 								>
@@ -86,7 +120,7 @@
 
 						<div class="setting-item">
 							<h3 class="setting-item-name">Nieuwe achtergrond</h3>
-							<div class="select">
+							<div class="input-select">
 								<select
 									v-model="currentSettings.wallpaper.wallpaperRefresh"
 								>
@@ -106,7 +140,7 @@
 					<div class="setting-group-content">
 						<div class="setting-item">
 							<h3 class="setting-item-name">Quote categorie</h3>
-							<div class="select">
+							<div class="input-select">
 								<select
 									v-model="currentSettings.quote.category"
 									class="text-capitalize"
@@ -127,14 +161,16 @@
 					<div class="setting-group-content">
 						<div class="setting-item">
 							<h3 class="setting-item-name">Nieuws slide snelheid</h3>
-							<input
-								type="range"
-								class="slider is-circle"
-								:min="settingsOptions.news.slideInterval.min"
-								:max="settingsOptions.news.slideInterval.max"
-								step="1000"
-								v-model="currentSettings.news.slideInterval"
-							><label>{{currentSettings.news.slideInterval / 1000}}s</label>
+							<div class="input-range-wrap">
+								<input
+									type="range"
+									class="slider is-circle input-range"
+									:min="settingsOptions.news.slideInterval.min"
+									:max="settingsOptions.news.slideInterval.max"
+									step="1000"
+									v-model="currentSettings.news.slideInterval"
+								><label>{{currentSettings.news.slideInterval / 1000}}s</label>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -232,6 +268,14 @@ export default {
 </script>
 
 <style scoped>
+.settings-page {
+	font-size: 0.875rem;
+	position: fixed;
+	top: 0; bottom: 0;
+	left: 0; right: 0;
+	background-color: rgba(10, 10, 10, 0.9);
+}
+
 .text-capitalize {
 	text-transform: capitalize;
 }
@@ -260,13 +304,6 @@ label > input {
 	margin-bottom: 0.25rem;
 }
 
-.settings-page {
-	font-size: 1rem;
-	position: fixed;
-	top: 0; bottom: 0;
-	left: 0; right: 0;
-	background-color: rgba(10, 10, 10, 0.9);
-}
 
 .settings-content {
 	height: 100%;
@@ -276,8 +313,8 @@ label > input {
 }
 
 .settings-content-inner {
-	width: 80%;
-	max-width: 1000px;
+	width: 70%;
+	max-width: 900px;
 	min-width: 700px;
 	margin: auto;
 }
@@ -304,8 +341,8 @@ h2 {
 	border-bottom: 1px solid rgba(255, 255, 255, 0.4);
 }
 
-.setting-group:not(:last-of-type) {
-	margin-bottom: 0.7rem;
+.setting-group {
+	margin-bottom: 1rem;
 }
 
 .setting-group > h2 {
@@ -315,4 +352,59 @@ h2 {
 .setting-group-content {
 	flex: 1 1 auto;
 }
+
+.input-select, .input-text, .input-range-wrap, .checkbox-group-optional-text {
+	width: 100%;
+	max-width: 500px!important;
+}
+
+.input-range-wrap {
+	display: flex;
+}
+
+.input-range-wrap > label {
+	flex: 0 0 2rem;
+}
+
+.input-range-wrap > .input-range {
+	flex: 1 1 auto;
+}
+
+.checkbox-group-optional-text {
+	display: flex;
+	align-items: center;
+}
+
+.checkbox-group-optional-text .input-checkbox {
+	flex: 0 0 auto;
+}
+
+.checkbox-group-optional-text > label {
+	flex: 1 1 90%;
+	display: flex;
+	align-items: center;
+}
+
+.checkbox-group-optional-text > label > .input-text {
+	margin-left: 0.5rem;
+}
+
+.save-btn {
+	display: block;
+	margin: auto;
+	min-width: 8rem;
+	min-height: 3rem;
+	margin-top: 2rem;
+	margin-bottom: 3rem;
+	border-radius: 4px;
+	border: 2px solid white;;
+	background: white;
+	transition: all .2s ease;
+}
+
+.save-btn:hover {
+	background: black;
+	color: white;
+}
+
 </style>

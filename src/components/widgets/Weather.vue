5448109ec459f1ecfe6d3eb5e6025ee09ccb8ace
Michael van Meerwijk <michaelvanm@gmail.com>
2018-06-23 15:46:23 +0200

    Weather alignment

--- src/components/widgets/Weather.vue
+++ src/components/widgets/Weather.vue
@@ -54,52 +54,123 @@ export default {
 </script>
 
 <style scoped>
-.widget-weather {
+.w-align-left {
+	margin: 0 auto auto 0;
+	padding-left: 1em;
+	grid-template-columns: repeat(3, 1fr);
+}
+
+.w-align-center {
+	margin: 0 auto auto auto;
+	grid-template-columns: repeat(4, 1fr);
+}
+
+.w-align-right {
 	margin: 0 0 auto auto;
-	padding-right: 0.75em;
+	padding-right: 1em;
+	grid-template-columns: repeat(3, 1fr);
+}
+
+.w-align-left .temperature {
+	grid-column: 1;
+	justify-self: end;
+	text-align: right;
+}
+
+.w-align-center .temperature {
+	grid-column: 3 / 5;
+	text-align: center;
+	justify-self: start;
+	margin-right: 1.5rem;
+}
+
+.w-align-right .temperature {
+	grid-column: 3;
+	justify-self: end;
+	text-align: right;
+}
+
+.w-align-left .location {
+	text-align: left;	
+	justify-self: start;
+	grid-column: 1 / 4;
+}
+
+.w-align-center .location {
+	text-align: center;
+	justify-self: center;
+	grid-column: 1 / 5;
+}
+
+.w-align-right .location {
+	text-align: right;	
+	justify-self: end;
+	grid-column: 1 / 4;
+}
+
+.w-align-left .icon {
+	grid-column: 2;
+	align-self: center;
+	justify-self: end;
+}
+
+.w-align-center .icon {
+	grid-column: 1 / 3;
+	align-self: center;
+	justify-self: end;
+	margin-left: 1.5rem;
+}
+
+.w-align-right .icon {
+	grid-column: 2;
+	align-self: center;
+	justify-self: end;
+}
+
+.w-align-left .row-extended {
+	grid-column: 1 / 4;
+}
+
+.w-align-center .row-extended {
+	grid-column: 1 / 5;
+}
+
+.w-align-right .row-extended {
+	grid-column: 1 / 4;
+}
+
+.widget-weather {
 	position: relative;
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
 	grid-template-rows: auto auto auto;
 	cursor: pointer;
 	font-size: 1em;
 }
 
 .icon {
-	grid-column: 2;
 	grid-row: 1;
 	height: 3em;
 	width: 3em;
-	align-self: center;
-	justify-self: end;
 }
 
 .temperature {
-	grid-column: 3;
-	grid-row: 1;
-	justify-self: end;
-	text-align: right;
-	align-self: center;
 	font-size: 2em;
+	grid-row: 1;
+	align-self: center;
 }
 
 .location {
-	justify-self: end;
-	grid-column: 1 / 4;
 	grid-row: 2;
-	text-align: right;
 	color: rgba(255,255,255,1);
 	font-weight: 300;
 	font-size: 0.75em;
 	letter-spacing: 0.075em;
 	text-transform: uppercase;
-	text-align: center;
 	min-width: 5.75em;
 }
 
 .row-extended {
 	grid-row: 3;
-	grid-column: 1 / 4;
 	position: relative;
 	cursor: default;
 }

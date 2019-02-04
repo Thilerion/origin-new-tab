508c7afa7db3dbaa2ff87caac6f47e40137043df
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-04 12:45:31 +0100

    Watcher and persistence for activeWidgets/grid module

--- src/store/modules/grid/index.js
+++ src/store/modules/grid/index.js
@@ -1,17 +1,29 @@
 import { GRID_ROWS, GRID_COLS, WIDGET_TYPE_BG, WIDGET_TYPE_GRID } from '@/constants.js';
 
+import { ERR_REQUIRED, default as Validator } from '@/utils/validate.js';
+import { loadFromStorage, saveToStorage } from '@/utils/lsHelpers';
+import _debounce from 'lodash.debounce';
+
+const storageData = loadFromStorage('sp_widgets');
+
+// TODO: VALIDATORS ETC
+// TODO: FOR NOW, USE THIS DEFAULTS OBJECT
+const activeWidgetsDefaults = [
+	{
+		type: WIDGET_TYPE_BG,
+		name: 'WallpaperUnsplash'
+	}
+];
+
+const validatedData = [...activeWidgetsDefaults];
+
 export const gridModule = {
 	namespaced: true,
 
 	state: {
 		rows: GRID_ROWS,
 		cols: GRID_COLS,
-		activeWidgets: [
-			{
-				type: WIDGET_TYPE_BG,
-				name: 'WallpaperUnsplash'
-			},
-		]
+		activeWidgets: validatedData
 	},
 
 	getters: {
@@ -27,6 +39,25 @@ export const gridModule = {
 	}
 }
 
-export const activeWidgetsPersistPlugin = store => {
 
+
+export const activeWidgetsPersistPlugin = store => {
+	const reducerFn = state => (state.grid.activeWidgets);
+	const watchCb = newValue => {
+		console.log('Watcher triggered for "active widgets" module.');
+		saveToStorage('sp_widgets', newValue);
+	}
+	const debouncedCb = _debounce(
+		watchCb,
+		500,
+		{ maxWait: 10000 }
+	);
+
+	store.watch(
+		reducerFn,
+		debouncedCb,
+		{ deep: true }
+	);
+
+	console.log('Store watcher created for "ACTIVE WIDGETS" module, by plugin.');
 }

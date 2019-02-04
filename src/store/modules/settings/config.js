64f4aa56f50f7940750ed9eab7e776a1a438ea2a
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-04 12:23:39 +0100

    Move all config for settings into separate file

--- src/store/modules/settings/config.js
+++ src/store/modules/settings/config.js
@@ -1,6 +1,17 @@
 import { LANGUAGES, TIME_FORMATS } from '@/constants';
 
-export const generalSettingsConfig = {
+// Settings for each widget (not the instances, but the widget itself)
+import { widgetSettings } from '@/widgets';
+
+// TODO: settings for the activeWidgets
+// import .....
+
+// Validation class
+import { ERR_REQUIRED, default as Validator } from '@/utils/validate.js';
+
+import { loadFromStorage } from '@/utils/lsHelpers';
+
+const generalSettingsConfig = {
 	language: {
 		validate(val) {
 			return String(val) === val && LANGUAGES.includes(val);
@@ -24,3 +35,33 @@ export const generalSettingsConfig = {
 		defaultValue: () => ''
 	}
 }
+
+const settingConfigs = {
+	general: generalSettingsConfig,
+	...widgetSettings
+};
+
+/**
+ * Retrieve from LocalStorage
+ * Create validator class for every field in settingConfigs
+ * Validate all, setting defaults if needed
+ * Register store, setup watcher
+ */
+
+const storageData = loadFromStorage('sp_settings');
+
+const settingValidatorClasses = {};
+const validatedData = {};
+
+// Create validators for each setting category, and validate the stored data
+for (const [key, val] of Object.entries(settingConfigs)) {
+	const validator = new Validator(val);
+	settingValidatorClasses[key] = validator;
+	
+	const stored = (storageData && storageData[key]) || null;
+
+	const validated = validator.validate(stored);
+	validatedData[key] = validated;
+}
+
+export { validatedData };
--- src/store/modules/settings/index.js
+++ src/store/modules/settings/index.js
@@ -1,47 +1,8 @@
-// Settings for each widget (not the instances, but the widget itself)
-import { widgetSettings } from '@/widgets';
-
-// Settings for the user
-import { generalSettingsConfig } from './config';
-
-// TODO: settings for the activeWidgets
-// import .....
-
-// Validation class
-import { ERR_REQUIRED, default as Validator } from '@/utils/validate.js';
-
 // App helper functions
-import { loadFromStorage, saveToStorage } from '@/utils/lsHelpers';
+import { saveToStorage } from '@/utils/lsHelpers';
 import _debounce from 'lodash.debounce';
 
-const settingConfigs = {
-	general: generalSettingsConfig,
-	...widgetSettings
-};
-
-/**
- * Retrieve from LocalStorage
- * Create validator class for every field in settingConfigs
- * Validate all, setting defaults if needed
- * Register store, setup watcher
- */
-
-const storageData = loadFromStorage('sp_settings');
-
-const settingValidatorClasses = {};
-const validatedData = {};
-
-// Create validators for each setting category, and validate the stored data
-for (const [key, val] of Object.entries(settingConfigs)) {
-	const validator = new Validator(val);
-	settingValidatorClasses[key] = validator;
-	
-	const stored = (storageData && storageData[key]) || null;
-
-	const validated = validator.validate(stored);
-	validatedData[key] = validated;
-}
-
+import { validatedData } from './config';
 
 const settingsModule = {
 	namespaced: false,

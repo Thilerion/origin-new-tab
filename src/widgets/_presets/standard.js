3c0172033c4a3be49ed87d09565fb9b165745a16
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-11 13:27:12 +0100

    Add news widget to standard preset

--- src/widgets/_presets/standard.js
+++ src/widgets/_presets/standard.js
@@ -1,9 +1,16 @@
 export default [
 	{
-		name: 'WidgetClock',
-		x: 7,
-		y: 9,
+		name: "WidgetClock",
+		x: 12,
 		width: 18,
-		height: 6
+		y: 6,
+		height: 6,
+	},
+	{
+		name: "WidgetNews",
+		x: 12,
+		width: 18,
+		y: 1,
+		height: 2,
 	}
 ];

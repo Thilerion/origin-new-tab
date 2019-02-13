e5e272bc5d13b15d0e999252c8c45f52c9cccdf9
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-13 18:49:08 +0100

    Validator and form schema for generalSettings

--- src/store/settings/generalSettings.js
+++ src/store/settings/generalSettings.js
@@ -1,14 +1,38 @@
+import validators from '@/utils/form-generator/validators';
+
 export const general = {
 	username: {
 		name: 'username',
-		getDefault: () => 'My Username'
+		getDefault: () => 'My Username',
+		
+		type: 'input',
+		inputType: 'text',
+		label: 'Username',
+		validator: validators.string,
+		min: 1,
+		max: 30,
+		required: true
 	},
 	language: {
 		name: 'language',
-		getDefault: () => 'en'
+		getDefault: () => 'en',
+
+		type: 'radio',
+		values: [
+			{ name: 'English', value: 'en' },
+			{ name: 'Nederlands', value: 'nl' }
+		],
+		label: 'Language'
 	},
 	timeFormat: {
 		name: 'timeFormat',
-		getDefault: () => 'HH:mm'
+		getDefault: () => 'HH:mm',
+
+		type: 'radio',
+		values: [
+			{ name: '24-hour', value: 'HH:mm' },
+			{ name: '12-hour', value: 'h:mm a' }
+		],
+		label: 'Time Format'
 	}
 };

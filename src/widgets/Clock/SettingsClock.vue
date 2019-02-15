59d70a0a57a5e38b74f2b6c31177332f6e1e7a12
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-15 18:25:55 +0100

    Clock settings component

--- src/widgets/Clock/SettingsClock.vue
+++ src/widgets/Clock/SettingsClock.vue
@@ -1,12 +1,52 @@
 <template>
 	<div>
-		MY clock settings
+		<label for="date">Show date</label>
+		<input type="checkbox" name="date" id="date" v-model="showDate">
+
+		<label for="greeting">Show text greeting</label>
+		<input type="checkbox" name="greeting" id="greeting" v-model="showTextGreeting">
 	</div>
 </template>
 
 <script>
 export default {
-
+	props: {
+		settingOptions: {
+			type: Object,
+			required: true
+		}
+	},
+	computed: {
+		settings() {
+			return this.$store.state.settings.clock;
+		},
+		showDate: {
+			get() {
+				return this.settings.showDate;
+			},
+			set(value) {
+				this.updateSetting('showDate', value);
+			}
+		},
+		showTextGreeting: {
+			get() {
+				return this.settings.showTextGreeting;
+			},
+			set(value) {
+				this.updateSetting('showTextGreeting', value);
+			}
+		}
+	},
+	methods: {
+		updateSetting(key, value) {
+			this.$store.commit('updateSettings', {
+				key: 'clock',
+				settings: {
+					[key]: value
+				}
+			})
+		}
+	}
 }
 </script>
 

830415107ad15f9b24e67a1b240510217e8045b0
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-18 13:47:12 +0100

    GetCoordinates function for geolocation api

---
+++ src/utils/geolocation.js
@@ -0,0 +1,37 @@
+function getPosition() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject, {
+			timeout: 15000,
+			enableHighAccuracy: true
+		})
+	})
+};
+
+async function getCoordinates() {
+	try {
+		const position = await getPosition();
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		console.log({ latitude, longitude });
+		return { latitude, longitude };
+	} catch (e) {
+		const code = e.code;
+		if (code === 1) {
+			// No permission
+			console.warn("[Geolocation Error 1]: No permission granted.");
+		} else if (code === 2) {
+			// Location unavailable
+			console.warn("[Geolocation Error 2]: Location unavailable.");
+		} else if (code === 3) {
+			// Timeout expired
+			console.warn("[Geolocation Error 3]: Timeout expired.");
+		} else {
+			console.warn("[Geolocation Error]: unexpected error.\n", e);
+		}
+		return false;
+	}
+}
+
+
+
+export default getCoordinates;

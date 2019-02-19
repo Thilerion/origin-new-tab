052936a2efaf32caec76501525ea0d4f84667f63
Michael van Meerwijk <michaelvanm@gmail.com>
2019-02-19 20:31:33 +0100

    Implement range sliders in quickLink settings

--- src/widgets/QuickLinks/SettingsQuickLinks.vue
+++ src/widgets/QuickLinks/SettingsQuickLinks.vue
@@ -1,10 +1,63 @@
 <template>
-	
+	<div>
+		<FormRangeSlider
+			label="Amount"
+			name="amount"
+			:options="settingOptions.amount.options"
+			v-model="amount"
+		/>
+		<FormRangeSlider
+			label="Items per row"
+			name="amountPerRow"
+			:options="settingOptions.amountPerRow.options"
+			v-model="amountPerRow"
+		/>
+	</div>
 </template>
 
 <script>
+import FormRangeSlider from '@/components/form/FormRangeSlider.vue';
 export default {
-
+	components: {
+		FormRangeSlider
+	},
+	props: {
+		settingOptions: {
+			type: Object,
+			required: true
+		}
+	},
+	computed: {
+		settings() {
+			return this.$store.state.settings.quicklinks;
+		},
+		amount: {
+			get() {
+				return this.settings.amount;
+			},
+			set(value) {
+				this.updateSetting('amount', value);
+			}
+		},
+		amountPerRow: {
+			get() {
+				return this.settings.amountPerRow;
+			},
+			set(value) {
+				this.updateSetting('amountPerRow', value);
+			}
+		}
+	},
+	methods: {
+		updateSetting(key, value) {
+			this.$store.commit('updateSettings', {
+				key: 'quicklinks',
+				settings: {
+					[key]: value
+				}
+			})
+		}
+	}
 }
 </script>
 

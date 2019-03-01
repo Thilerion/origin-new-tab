d6e08d1a9bc24c1e0a340f4ef775bc4e9c5949f1
Michael van Meerwijk <michaelvanm@gmail.com>
2019-03-01 17:46:54 +0100

    Changelog loader and display

--- src/components/settings/SettingsWhatsNew.vue
+++ src/components/settings/SettingsWhatsNew.vue
@@ -1,15 +1,69 @@
 <template>
-	<div class="changelog">
-		
+	<div class="changelog" v-if="loaded">
+		<section class="log-item" v-for="(logItem, i) in sortedLog" :key="i">
+			<div class="log-header">
+				<h2 class="log-title"><span class="log-version">v{{logItem.version}}</span> - {{logItem.title}}</h2>
+				<div class="log-date">{{logItem.date | toDateString}}</div>
+			</div>
+			<ul class="log-points-list">
+				<li class="log-point" v-for="(item, j) in logItem.items" :key="j">{{item}}</li>
+			</ul>
+		</section>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			loaded: false,
+			logArray: []
+		}
+	},
+	computed: {
+		language() {
+			return this.$i18n.locale;
+		},
+		sortedLog() {
+			return [...this.logArray].sort((a, b) => {
+				return Date.parse(b.date) - Date.parse(a.date)
+			});
+		}
+	},
+	methods: {
+		loadLogs(lang) {
+			import(`@/i18n/changelog/${lang}`).then(log => {
+				console.log(log.default);
+				this.logArray = [...log.default];
+				this.loaded = true;
+			})
+		}
+	},
+	mounted() {
+		this.loadLogs(this.language);
+	},
+	watch: {
+		language(newValue, oldValue) {
+			console.log(`[WhatsNew component]: language changed from ${oldValue} to ${newValue}.`);
+		}
+	},
+	filters: {
+		toDateString(val) {
+			// TODO: localeDateString
+			return val;
+		}
+	}
 }
 </script>
 
 <style scoped>
+.log-item {
+	padding-bottom: 1em;
+}
 
+.log-header {
+	display: flex;
+	justify-content: space-between;
+	padding-bottom: 0.25em;
+}
 </style>
---
+++ src/i18n/changelog/en.js
@@ -0,0 +1,33 @@
+export default [
+	{
+		version: "0.15",
+		title: "Layout update",
+		type: "major",
+		date: "2018-06-29",
+		items: [
+			"It is now possible to choose from several layout presets",
+			"Widgets can be vertically aligned, in addition to the previously available horizontal alignment option",
+			"New options for the clock widget have been added"
+		]
+	},
+	{
+		version: "0.16",
+		title: "Rework",
+		type: "major",
+		date: "2019-02-21",
+		items: [
+			"The extension has been rebuilt from the ground up. This should, among others, improve the speed at which we can add new widgets",
+			"An entirely new settings page has been created, making it easier to find the specific setting you are looking for",
+			"Customization of the dashboard and widgets has been overhauled. It is now much easier to add new widgets and to move and resize them"
+		]
+	},
+	{
+		version: "0.16.1",
+		type: "minor",
+		title: "Minor fixes",
+		date: "2019-02-22",
+		items: [
+			"Minor bug fixes and improvements"
+		]
+	}
+]
